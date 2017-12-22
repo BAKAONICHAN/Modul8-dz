185421123704bfbb573dbd878c002d05df3f99e1
Nurik <tanatov1989@mail.ru>
2017-12-22 17:36:57 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Array.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Array
+    {
+        public int[] _Array;
+        
+        public int degree;
+        public Array()
+        {
+            degree = 2;
+            _size= 0;
+            _Array = new int[_size];
+        }
+        public Array(int size,int _degree)
+        {
+            _size = size;
+            _Array = new int[_size];
+            degree = _degree;
+        }
+        public int _size { get; set; }
+        public int this[int i]
+        {
+            get
+            {
+                return _Array[i];
+            }
+            set
+            {
+                _Array[i] = (int)Math.Pow((double)value, degree);
+            }
+            
+          
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int sizeOfArray=3;
+            Array array = new Array(sizeOfArray,2);
+            WriteLine("Первый ряд чисел");
+            array[0] = 12;
+            array[1] = 2;
+            array[2] = 3;
+            for (int i=0;i<array._size;i++)
+            {                
+            WriteLine(array[i]);
+            }
+            ReadLine();
+        }
+    }
+}
